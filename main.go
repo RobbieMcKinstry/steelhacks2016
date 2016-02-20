8251939da9907a639a4478b0fe01619a47c30a90
Robbie McKinstry <rem89@pitt.edu>
2016-02-20 00:34:43 -0500

    correcting a filepath error

--- main.go
+++ main.go
@@ -18,14 +18,7 @@ func main() {
 	r.HandleFunc("/upload", UploadGetHandler).Methods("GET")
 	r.HandleFunc("/upload", UploadGetHandler).Methods("POST")
 
-	s := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-	r.PathPrefix("/static/").Handler(s)
-	//r.PathPrefix("/static").Handler(http.FileServer(http.Dir("./app/static/")))
-	//fs := http.FileServer(http.Dir("app/static"))
-	//r.Handle("/static/", http.StripPrefix("/static/", fs))
-	//r.Handle("/static/", http.StripPrefix("/static/", fs))
-	//r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/tmp"))))
-	//r.Handle("/static", http.FileServer(http.Dir("app/static")))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.Handle("/", r)
 	log.Println("Running on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -33,14 +26,14 @@ func main() {
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received a request to /")
-	name := "app/static/html/index.html"
+	name := "static/html/index.html"
 	http.ServeFile(w, r, name)
 }
 
 func ProjectHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received a request to /project")
 	//p, _ := loadPage(title)
-	t, err := template.ParseFiles("app/templates/project.html.tmpl")
+	t, err := template.ParseFiles("templates/project.html.tmpl")
 	if err != nil {
 		log.Error(err)
 	}
