61194a1f3cf5d83509ac10ee67ac2d44f2c5b881
Robbie McKinstry <rem89@pitt.edu>
2016-02-20 00:01:10 -0500

    Adding a prject route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
@@ -13,13 +14,24 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/project", ProjectHandler)
 	http.Handle("/", r)
 	log.Println("Running on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Received a request!")
+	log.Println("Received a request to home!")
 	name := "app/static/html/index.html"
 	http.ServeFile(w, r, name)
+}
+
+func ProjectHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received a request to Project!")
+	//p, _ := loadPage(title)
+	t, err := template.ParseFiles("app/templates/project.html.tmpl")
+	if err != nil {
+		log.Error(err)
+	}
+	t.Execute(w, nil)
 }
